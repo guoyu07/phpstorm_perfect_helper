f927870edcb0d74edbf01cd03f2461db2bc28f6c
justmd5 <ding21st@gmail.com>
2016-05-21 01:42:19 +0800

    加入jsond提示

---
+++ jsond_auto_complete.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * phpstorm_perfect_helper
+ *jetbrains idea phpstorm helper &&php_perfect_helper
+ * 本文件使用方式：
+ *
+ * Yaf开发，在IDE中打开/导入本文件即可
+ * 如果IDE自带 Include Path 功能(如：PhpStorm)，设置该文件路径即可.
+ *
+ * PhpStorm 另一种方法:
+ * WinRAR打开 <Phpstorm_Dir>/plugins/php/lib/php.jar 文件
+ * 复制 Yaf.namespace.php 到路径：com/jetbrains/php/lang/psi/stubs/data/
+ * 保存文件，重启Phpstorm.
+ *
+ * PS:替换前请备份php.jar 若发生错误便于恢复
+ *
+ * @author ding21st@gmail.com
+ * @copyright Copyright (c) 2016
+ */
+/**
+ * pack the data with msgpack
+ * @param string|string[] $NeedPackedInput
+ * @return bool|string
+ */
+/**
+ * jsond auto_complate
+ * @author akmumu
+ */
+/**
+ * @param mixed $value
+ * @return string
+ */
+function jsond_encode( $value) {}
+/**
+ * @param string $json
+ * @param bool|false $assoc
+ * @return mixed
+ */
+function jsond_decode( $json,  $assoc = false) {}
